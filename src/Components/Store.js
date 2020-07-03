3eb8e7af3e6d690f435dc6a35a5304d2900c71c3
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-07-02 21:28:14 -0500

    Componente Store

---
+++ src/Components/Store.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Store = () => {
+
+    return (
+        <div className="task">
+            This actually isnt a store, just a file to new branch commit
+        </div >
+    );
+
+}
+
+export default Store;
